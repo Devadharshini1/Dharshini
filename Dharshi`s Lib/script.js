de344be01dced06142abfa47f3c02b4071f01bb8
Devadharshini1 <144244675+Devadharshini1@users.noreply.github.com>
2024-08-24 19:11:07 +0530

    Add files via upload

---
+++ Dharshi`s Lib/script.js
@@ -0,0 +1,30 @@
+//selectng popup box popup overlay and button
+var popupoverlay=document.querySelector(".popup-overlay")
+var popupbox=document.querySelector(".popup-box")
+var addpopupbutton=document.getElementById("add-popup-button")
+
+addpopupbutton.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+     popupbox.style.display="block"
+})
+var cancelpopup=document.getElementById("cancel-popup")
+cancelpopup.addEventListener("click",function(event){
+  event.preventDefault()
+   popupoverlay.style.display="none"
+     popupbox.style.display="none"
+
+})
+//select container,addbook,book title input,author,description
+var container=document.querySelector(".container")
+var addbook=document.querySelector("add-book")
+var booktitleinput=document.getElementById("book-title-input")
+var bookauthorinput=document.getElementById("book-author-input")
+var bookdescriptioninput=document.getElementById("book-description-input")
+
+addbook.addEventListener("click",function(event){
+    event.preventDefault()
+    var div=document.createElement("div")
+    div.setAttribute("class","book-container")
+    div.innerHTML=`<h1>${booktitleinput.value}</h1>`
+    container.append(div)
+})
